8dd3e99844b7ea73ed14df7255ca5fe02cb42a22
Marlon <luciousmc@icloud.com>
2020-08-27 13:48:40 -0700

    adds meta info

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,28 @@
 import styles from "./layout.module.css";
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassests.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+    </div>
+  );
 }
 
 export default Layout;
