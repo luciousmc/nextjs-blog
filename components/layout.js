70dce073bf241877d8c523b2dc701bb1ff4f3fcb
Marlon <luciousmc@icloud.com>
2020-08-27 14:13:06 -0700

    styled to a personal profile

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,9 @@ import Head from "next/head";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
+const name = "Marlon Clay";
+export const siteTitle = "Next.js Sample Website";
+
 function Layout({ children, home }) {
   return (
     <div className={styles.container}>
@@ -21,6 +24,43 @@ function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 clasName={utilStyles.heading2X1}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  clasName={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.BackToHome}>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
